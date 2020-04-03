2dca6235f2c6dff9ad1327a4a9fa23fcd0df8b65
Marione Espino <marione.espino@ibiz-solutions.se>
2020-04-04 00:27:48 +0200

    fix of front-end

--- ME.Account.Web/ME.Account.Web/Controllers/HomeController.cs
+++ ME.Account.Web/ME.Account.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ME.Account.Web.Core.Business;
+using ME.Account.Web.Core.Contracts;
+using ME.Account.Web.Models.api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,25 @@ namespace ME.Account.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private ICustomerInfoService _customerInfoService = new CustomerInfoService();
+
         public ActionResult Index()
         {
-            return View();
+            var customerRequest = new CustomerRequest()
+            {
+                customerId = string.Empty,
+                initialCredit = 0
+            };
+ 
+            return View(customerRequest);
+        }
+
+        [HttpPost]
+        public ActionResult GetAccount(CustomerRequest customerRequest)
+        {
+           var customerAccount = _customerInfoService.RegisterAmount(customerRequest.customerId, customerRequest.initialCredit);
+
+            return RedirectToAction("Index", "Transactions", customerAccount);
         }
 
         public ActionResult About()
---
+++ ME.Account.Web/ME.Account.Web/Controllers/TransactionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ME.Account.Web.Core.Contracts;
+using ME.Account.Web.Core.Business;
+using ME.Account.Web.Models.api;
+
+namespace ME.Account.Web.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private ITransactionInfoService transactionInfoService = new TransactionInfoService();
+
+        public ActionResult Index(CustomerAccount customerAccount)
+        {
+            var transResponse = transactionInfoService.GetCustomerTransInfo(customerAccount.CustomerId, DateTime.Now, DateTime.Now);
+            return View(transResponse);
+        }
+    }
+}
